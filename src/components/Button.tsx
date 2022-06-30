042181814d94d6fd78f488466bccea7e78f77585
Iqbal Ibrahim <iqbal.ibrahim@deadhappy.com>
2022-06-30 01:05:16 +0100

    feat: added classname prop to button; add copy icon svg

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,17 +4,19 @@ interface ButtonProps {
     text: string;
     onClickHandler: any;
     variation: string;
+    className: string;
 }
 
-const Button = ({text, onClickHandler} : ButtonProps) => {
+const Button = ({text, onClickHandler, className} : ButtonProps) => {
     
     return (
-        <button className="bg-sky-500 hover:bg-sky-600 text-white font-bold py-2 px-4 rounded-sm" onClick={onClickHandler}>{text}</button>
+        <button className={`bg-sky-500 hover:bg-sky-600 text-white font-bold py-2 px-4 rounded-sm ${className}`} onClick={onClickHandler}>{text}</button>
     )
 }
 
 Button.defaultProps = {
-    variation: 'primary'
+    variation: 'primary',
+    className: ''
 }
 
 export default Button;
