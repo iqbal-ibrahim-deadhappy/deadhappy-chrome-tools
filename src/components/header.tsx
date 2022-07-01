18235157bc53f788b3b833d5de5b2ebbba85bbbe
Iqbal Ibrahim <iqbal.ibrahim@deadhappy.com>
2022-07-01 16:26:32 +0100

    feat: added quick links with icons in header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { User } from "../types";
+import NavigateTo from "./NavigateTo";
 
 interface HeaderProps {
   user: User;
@@ -37,6 +38,32 @@ export const Header = (props: HeaderProps) => {
           <div className="text-right">{new Date().toLocaleDateString()}</div>
           <div className="text-right font-bold">{currentTime}</div>
         </div>
+      </div>
+      <div className="container grid grid-flow-col grid-cols-8 gap-4 py-3 items-center justify-center">
+            <div className="text-left font-bold text-md">
+              Quick Links
+            </div>
+            <div className="justify-center flex hover:cursor-pointer" onClick={() => NavigateTo('https://gitlab.com', true)}>
+              <img src="/assets/gitlab.svg" className="h-5"/>
+            </div>
+            <div className="justify-center flex hover:cursor-pointer" onClick={() => NavigateTo('https://deadhappy.awsapps.com/start/#/', true)}>
+              <img src="/assets/aws.png" className="h-6"/>
+            </div>
+            <div className="justify-center flex hover:cursor-pointer" onClick={() => NavigateTo('https://notion.so', true)}>
+              <img src="/assets/notion.png" className="h-5"/>
+            </div>
+            <div className="justify-center flex hover:cursor-pointer" onClick={() => NavigateTo('https://stripe.com', true)}>
+              <img src="/assets/stripe.png" className="h-6"/>
+            </div>
+            <div className="justify-center flex hover:cursor-pointer" onClick={() => NavigateTo('https://linear.app', true)}>
+              <img src="/assets/linear.png" className="h-6"/>
+            </div>
+            <div className="justify-center flex hover:cursor-pointer" onClick={() => NavigateTo('https://miro.com', true)}>
+              <img src="/assets/miro.png" className="h-6"/>
+            </div>
+            <div className="justify-center flex hover:cursor-pointer" onClick={() => NavigateTo('https://sentry.io/auth/login/deadhappy/', true)}>
+              <img src="/assets/sentry.png" className="h-6"/>
+            </div>
       </div>
     </>
   );
